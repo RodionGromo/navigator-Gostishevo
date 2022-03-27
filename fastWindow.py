b340782ca2c9d6f4e520bb964724612315130a5b
TheGrom <49732296+RodionGromo@users.noreply.github.com>
2022-03-27 12:32:27 +0300

    update

--- fastWindow.py
+++ fastWindow.py
@@ -2,26 +2,36 @@
 from tkinter import Tk,Canvas
 import ctypes
 import markers
-from PIL import ImageTk
+from PIL import ImageTk,Image
 
 class fastWindow:
-	def __init__(self,windowTitle="Default Title",image=None):
+	def __init__(self,windowTitle="Default Title",imagePath=None):
 		self.windowTitle = windowTitle
-		self.image = image
+		self.imagePath = imagePath
 		self.window = None
 		self.markers = []
 		self.canvas = None
 		self.created = False
+		self.image = None
 		self.imageData = None
 		user32 = ctypes.windll.user32
 		self.screenX = user32.GetSystemMetrics(0)
 		self.screenY = user32.GetSystemMetrics(1)
+		self.binded = False
+		self.motionBind = None
+		self.clickBind = None
 
 	def getCenter(self,windowsize):
 		windowX,windowY = windowsize
 		size = f"{windowX}x{windowY}+{abs(int((self.screenX/2) - (windowX/2)))}+{abs(int((self.screenY/2) - (windowY/2)))}"
 		print(size)
 		return size
+
+	def readImage(self):
+		if(self.imagePath != None):
+			print("Reading image...")
+			self.image = Image.open(self.imagePath)
+			self.resize_image()
 
 	def resize_image(self):
 		if(self.image.height > self.screenY):
@@ -30,25 +40,39 @@ class fastWindow:
 			self.image = self.image.resize((self.screenX,self.image.height))
 
 	def create_window(self):
+		self.created = True
+		self.readImage()
 		self.window = Tk()
+		self.window.wm_attributes("-transparent","purple")
 		self.window.title(self.windowTitle)
 		if(self.image != None):
-			self.resize_image()
 			self.imageData = ImageTk.PhotoImage(self.image)
 			self.window.geometry(self.getCenter((self.imageData.width(),self.imageData.height())))
-			self.canvas = Canvas(master=self.window)
+			self.canvas = Canvas(width=self.imageData.width(),height=self.imageData.height())
 			self.canvas.pack()
 			self.canvas.create_image(0,0,image=self.imageData,anchor="nw")
 
 	def start_window(self):
 		self.window.mainloop()
 
-	def kickStart(self):
-		self.create_window()
-		self.start_window()
-
 	def passCallback():
 		pass
+
+	def destroySelf(self):
+		if(self.window != None):
+			if(self.binded):
+				self.canvas.unbind("<Motion>",self.motionBind)
+				self.canvas.unbind("<Button-1>",self.clickBind)
+			self.created = False
+			self.window.destroy()
+
+	def updMoveMarkers(self,event):
+		for mark in self.markers:
+			mark.handle(event=event)
+
+	def clickMarkers(self,event):
+		for mark in self.markers:
+			mark.handleClick(event)
 
 	def add_marker(self,x=0,y=0,r=5,color="white",text="Default Text",callback=None):
 		if self.created:
@@ -56,8 +80,15 @@ class fastWindow:
 				self.markers.append(markers.marker(posX=x,posY=y,diameter=r,color=color,textDesc=text,callback=self.passCallback,canvas=self.canvas,window=self.window))
 			else:
 				self.markers.append(markers.marker(posX=x,posY=y,diameter=r,color=color,textDesc=text,callback=callback,canvas=self.canvas,window=self.window))
+			if(self.binded == False):
+				self.bindMotions()
 		else:
 			print("Window is not created! Create window then try again")
+
+	def bindMotions(self):
+		self.binded = True
+		self.motionBind = self.canvas.bind("<Motion>",self.updMoveMarkers)
+		self.clickBind = self.canvas.bind("<Button-1>",self.clickMarkers)
 
 
 
--- mainFile.py
+++ mainFile.py
@@ -1,6 +1,79 @@
 #main gostishevo
-import fastWindow
-from PIL import Image,ImageTk
-testImage = Image.open("./assets/1.png")
-testWindow = fastWindow.fastWindow(windowTitle="Ура библиотека работает",image=testImage)
-testWindow.kickStart()
+from fastWindow import fastWindow
+from tkinter import Label,font,Button
+
+def mainWindow():
+	def startAdventure():
+		mW.destroySelf()
+		createMap()
+	mW = fastWindow(windowTitle="Навигатор Гостищево")
+	mW.create_window()
+	lb1 = Label(master=mW.window,text="Добро пожаловать в...")
+	lb1.configure(font=font.Font(family="Times New Roman",size=14,weight="bold"))
+	lb1.pack()
+	lb2 = Label(master=mW.window,text='Навигатор Гостищево')
+	lb2.configure(font=font.Font(family="Times New Roman",size=48,weight="normal"))
+	lb2.pack()
+	btn1 = Button(master=mW.window,text="Начать путешествие!",command=startAdventure)
+	btn1.pack()
+	mW.start_window()
+
+def createMap():
+	def exitApp():		
+		mapWindow.destroySelf()
+
+	def firstMem():
+		mapWindow.destroySelf()
+		firstMemorial()
+
+	def secondMem():
+		mapWindow.destroySelf()
+		secondMemorial()
+
+	mapWindow = fastWindow(windowTitle="Карта",imagePath="./assets/map.png")
+	mapWindow.create_window()
+	mapWindow.add_marker(x=475, y=322, r=15, text="Памятник морякам 281 полка", callback=firstMem)
+	mapWindow.add_marker(x=879, y=175, r=15, text="Памятник воинам, погибшим в ВОВ",callback=secondMem)
+	mapWindow.add_marker(x=275, y=729, r=25, color="red", text="Выйти", callback=exitApp)
+	mapWindow.start_window()
+
+def thirdMemorial():
+	def goBack():
+		memorialWindow.destroySelf()
+		createMap()
+
+	memorialWindow = fastWindow(windowTitle="_",imagePath="./assets/_.png")
+	memorialWindow.create_window()
+	lb1t = "_"
+	lb1 = Label(master=memorialWindow.window,text=lb1t,justify='left',wraplength=180)
+	lb1.place(x=0,y=0)
+	memorialWindow.add_marker(x=0,y=0,r=15,text="Назад",callback=goBack)
+	memorialWindow.start_window()
+
+def secondMemorial():
+	def goBack():
+		memorialWindow.destroySelf()
+		createMap()
+
+	memorialWindow = fastWindow(windowTitle="Памятник воинам, погибшим в ВОВ",imagePath="./assets/1.png")
+	memorialWindow.create_window()
+	lb1t = "Памятник воинам, погибшим в ВОВ:\n5 декабря 1941 года немцы расстреляли около 50 советских солдат на улице Кирова в с. Гостищево."
+	lb1 = Label(master=memorialWindow.window,text=lb1t,justify='left',wraplength=180)
+	lb1.place(x=374,y=70)
+	memorialWindow.add_marker(x=444,y=704,r=15,text="Назад",callback=goBack)
+	memorialWindow.start_window()
+
+def firstMemorial():
+	def goBack():
+		memorialWindow.destroySelf()
+		createMap()
+
+	memorialWindow = fastWindow(windowTitle="Памятник морякам 281 полка",imagePath="./assets/6.png")
+	memorialWindow.create_window()
+	lb1 = Label(master=memorialWindow.window,text="Памятник морякам 281 полка:\nОжесточенные бои с 7 по 15 июля 1943 года в районе с. Гостищево за железную дорогу.",justify="left",wraplength=200)
+	lb1.place(x=356,y=70)
+	memorialWindow.add_marker(x=500,y=721,r=15,text="Назад",callback=goBack)
+	memorialWindow.start_window()
+
+if __name__ == '__main__':
+	mainWindow()
